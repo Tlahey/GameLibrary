d869c378fadba3fc5491216e47faa1d043ccfff4
Antoine <antoine.vendeville@gmail.com>
2020-04-01 01:01:01 +0200

    test log configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ServiceConfig from './config/server.config';
+import DBConfiguration from './config/database.config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,7 @@ async function bootstrap() {
     origin: ServiceConfig().CORS_ORIGIN,
     methods: ServiceConfig().CORS_METHODS,
   })
+  console.log(DBConfiguration());
   await app.listen(ServiceConfig().SERVER_PORT);
 }
 bootstrap();
